385834781136c8feeb06ad045e56058e66634900
thushara-workspace-admin <thusharathilan123@gmail.com>
2025-05-07 00:08:34 +0530

    updated axios instance file backend port

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'https://5cd8-2402-4000-1333-4455-d3a-817-1e62-a54b.ngrok-free.app',
+    baseURL: 'https://8d87-2402-4000-1333-4455-d3a-817-1e62-a54b.ngrok-free.app',
     timeout: 30000,
     headers: { 'Content-Type': 'application/json' }
 })
