0bd807ec69fff4309bb8201c71687c0f2d949ef6
thushara-workspace-admin <thusharathilan123@gmail.com>
2025-05-07 16:50:11 +0530

    axios instace updated

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'https://8d87-2402-4000-1333-4455-d3a-817-1e62-a54b.ngrok-free.app',
+    baseURL: 'https://4765-175-157-232-6.ngrok-free.app',
     timeout: 30000,
     headers: { 'Content-Type': 'application/json' }
 })
