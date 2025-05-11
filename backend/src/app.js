6fce26df1133496561b726a101c4441bd70b0514
thushara-workspace-admin <thusharathilan123@gmail.com>
2025-05-12 01:39:10 +0530

    back end cors link updated

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,7 +5,7 @@ import rootRoute from "./routes/root.route.js";
 const app = express()
 
 app.use(express.json())
-app.use(cors({ origin: "https://genuine-gnome-0fe49b.netlify.app", credentials: true }))
+app.use(cors({ origin: "https://thushara-thilan-portfolio.netlify.app", credentials: true }))
 
 //routes
 app.use('/post', rootRoute)
