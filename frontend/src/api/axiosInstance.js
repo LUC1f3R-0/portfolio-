cbf2d25db397b3ea2a8c8dbe6b4842a9b5979c75
thushara-workspace-admin <thusharathilan123@gmail.com>
2025-05-14 10:46:22 +0530

    updated back end link

--- frontend/src/api/axiosInstance.js
+++ frontend/src/api/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const axiosInstance = axios.create({
-    baseURL: 'https://205b-212-104-231-7.ngrok-free.app',
+    baseURL: 'https://9fc4-212-104-231-7.ngrok-free.app',
     timeout: 30000,
     headers: { 'Content-Type': 'application/json' }
 })
